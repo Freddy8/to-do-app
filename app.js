15d9c194e5fafa3c2d703d04d7f2bf7b211cf0b4
Freddy8 <Contactfreddy11@Gmail.com>
2018-02-14 20:22:36 -0800

    done

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 
 
 function onReady() {
+  let id = 0;
 let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
+
   function createNewToDo() {
-  if (!newToDoText.value) { return; }
-      toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
+    if (!newToDoText.value) { return; }
+        toDos.push({
+        title: newToDoText.value,
+        complete: false,
+        id: id++,
+      });
 
-      newToDoText.value = '';
+        newToDoText.value = '';
 
-      renderTheUI();
+        renderTheUI();
    }
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
@@ -25,15 +28,25 @@ let toDos = [];
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const debutton = document.createElement('button');
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-     });
+      newLi.appendChild(debutton);
 
-   }
+      debutton.addEventListener('click', () =>{
+        deleteToDo(toDo.id);
+      });
+   });
+ }
+ function deleteToDo(id) {
+
+   toDos = toDos.filter(item => item.id !== id);
+   renderTheUI();
+ };
  addToDoForm.addEventListener('submit', event => {
       event.preventDefault();
       createNewToDo();
@@ -42,12 +55,13 @@ let toDos = [];
 renderTheUI();
 
 
+  }
 
 }
 
+
+
+
 window.onload = function() {
   onReady();
-
-
-
 };
